baa3e746814164bdae3dab8f669614c22b6bb2eb
Christopher Lam <christopher.lam@students.mq.edu.au>
2018-10-22 17:20:16 +1100

    update users with edit tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update 
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+    # Handle a successful update
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
   private
+  # to prevent mass assignment vulnerability
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
